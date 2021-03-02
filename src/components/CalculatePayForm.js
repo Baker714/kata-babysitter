e5a3213495a170c00ead026b40bd88cf5beadcbc
Baker714 <baker714@github.com>
2021-03-02 13:52:38 -0500

    Added basic form validation on times

--- src/components/CalculatePayForm.js
+++ src/components/CalculatePayForm.js
@@ -9,19 +9,25 @@ class CalculatePayForm extends Component {
 
   constructor(props){
     super(props);
-    this.state = {startTime: '', bedTime: '', endTime: '', amountToCharge: '0'};
+    this.state = {startTime: null, bedTime: null, endTime: null, amountToCharge: '0'};
     this.calculatePay = this.calculatePay.bind(this);
+    this.startTimeRef = React.createRef();
     //this.amountToCharge = React.createRef(); tried to create ref to update amountToCharge value, found better way with querySelector
   }
 
   changeStartTimeField(event){
     console.log(event);
-    if(event < "17:00"){
-      alert("Start Time must be after 5:00P.M.");
-      this.setState({startTime: ''});
-    }
-    else
-      this.setState({startTime: event});
+    this.setState({startTime: event});
+    //if(event < "17:00"){
+      //alert("Start Time must be after 5:00P.M.");
+      // console.log(document.getElementById("startTimePicker").value); logging
+      // console.log(document.querySelector("#startTimePicker").value);
+      //this.refs.startTimePicker.value = ""; setting up the ref didn't quite work either
+      //this.setState({startTime: null});
+      //console.log(this.startTimeRef.value);
+      //this.startTimeRef.current.value = null;
+      //document.querySelector('startTimePicker').value = ""; tried to set value to empty, couldn't grab it this way
+    //}
   }
 
   changeBedTimeField(event){
@@ -44,15 +50,31 @@ class CalculatePayForm extends Component {
     let bedTime = this.state.bedTime;
     let endTime = this.state.endTime;
 
-    this.validateTimes(startTime, bedTime, endTime);
-
-    document.querySelector('#amountToCharge').innerText = amountToCharge;
+    if(this.validateTimes(startTime, bedTime, endTime))
+      document.querySelector('#amountToCharge').innerText = amountToCharge;
   }
 
   validateTimes(startTime, bedTime, endTime) {
 
+    if(startTime === null || bedTime === null || endTime === null){
+      alert("Please fill in all fields before submitting");
+      return false;
+    }
 
+    if(startTime < "17:00" && startTime > "3:00"){
+      alert("Start Time must be 5:00P.M or later (Cutoff at 3:00A.M.)");
+      return false;
+    }
 
+    if(bedTime < startTime || bedTime > endTime){
+      alert("Bed Time must be between Start Time and End Time");
+      return false;
+    }
+
+    if(endTime < "18:00" && endTime > "4:00"){
+      alert("End Time must be 4:00A.M or earlier (Cutoff at 6:00P.M.)");
+      return false;
+    }
   }
 
   render() {
@@ -60,7 +82,7 @@ class CalculatePayForm extends Component {
       <div>
         <form onSubmit={this.calculatePay} id="calcPayForm">
           <label>Start Time:</label>
-          <TimePicker id="startTimePicker" disableClock={true} value={this.state.startTime} onChange={this.changeStartTimeField.bind(this)}/>
+          <TimePicker ref={this.startTimeRef} disableClock={true} value={this.state.startTime} onChange={this.changeStartTimeField.bind(this)}/>
           <br/>
           <br/>
           <label>Bed Time:</label>
