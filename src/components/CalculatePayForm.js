2c490e0a20fe5e257dbefbb611c6491ab679673b
Baker714 <baker714@github.com>
2021-03-02 15:59:29 -0500

    added time array of maximum time babysitting in order to calculate time spent babysitting

--- src/components/CalculatePayForm.js
+++ src/components/CalculatePayForm.js
@@ -13,6 +13,7 @@ class CalculatePayForm extends Component {
     this.calculatePay = this.calculatePay.bind(this);
     this.startTimeRef = React.createRef();
     //this.amountToCharge = React.createRef(); tried to create ref to update amountToCharge value, found better way with querySelector
+    const timeArray = ["17:00","18:00","19:00","20:00","21:00","22:00","23:00","00:00","01:00","02:00","03:00","04:00"]; //creating timeArray list to properly calculate hours babysitting
   }
 
   changeStartTimeField(event){
@@ -50,8 +51,11 @@ class CalculatePayForm extends Component {
     let bedTime = this.state.bedTime;
     let endTime = this.state.endTime;
 
-    if(this.validateTimes(startTime, bedTime, endTime))
+    if(this.validateTimes(startTime, bedTime, endTime)){
+      amountToCharge += (bedTime-startTime)*12;
+      amountToCharge += ("24:00"-bedTime);
       document.querySelector('#amountToCharge').innerText = amountToCharge;
+    }
   }
 
   validateTimes(startTime, bedTime, endTime) {
@@ -65,15 +69,15 @@ class CalculatePayForm extends Component {
       alertMessage += "Start Time must be between 5:00P.M and Bed Time\n\n";
     }
 
-    if(bedTime < startTime || bedTime > endTime){
+    if(bedTime < startTime || (bedTime > endTime && bedTime)){
       alertMessage += "Bed Time must be between Start Time and End Time\n\n";
     }
 
-    if(bedTime >= "12:00"){
+    if(bedTime >= "00:00"){
       alertMessage += "Bed Time must be before Midnight\n\n";
     }
 
-    if(endTime < "12:00" && endTime > "04:00"){
+    if(endTime < "24:00" && endTime > "04:00"){
       alertMessage += "End Time must be between Midnight and 4:00A.M\n\n";
     }
 
